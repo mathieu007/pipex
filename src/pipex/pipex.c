c8b9218ad6427f1763b04c3c4527b96ca8243b00
Mathieu Roy <mathieuroy1983@gmail.com>
2023-03-09 12:46:06 -0500

    commit

--- src/pipex/pipex.c
+++ src/pipex/pipex.c
@@ -6,12 +6,14 @@
 /*   By: mroy <mroy@student.42.fr>                  +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/02/26 08:02:59 by math              #+#    #+#             */
-/*   Updated: 2023/03/09 10:47:29 by mroy             ###   ########.fr       */
+/*   Updated: 2023/03/09 12:46:04 by mroy             ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "pipex.h"
 
+/// @brief test
+/// @param
 void	usage(void)
 {
 	printf("Invalid argument count.\n");
