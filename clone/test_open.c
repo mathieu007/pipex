ab47e35b2a40abb10ba593508661a3d4e9d3a24d
Mathieu Roy <mathieuroy1983@gmail.com>
2023-03-17 10:43:15 -0400

    commit

---
+++ clone/test_open.c
@@ -0,0 +1,12 @@
+#include <fcntl.h>
+#include <stdbool.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <sys/wait.h>
+#include <unistd.h>
+
+int	main(void)
+{
+	printf("fd:%lu", sizeof(bool));
+}
